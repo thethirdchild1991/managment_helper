3083186b36cd2e5a1ff40bc1be8a7fdcdb8af23b
denis <denis.dylko@gmail.com>
2019-04-17 22:56:12 +0300

    .jest try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
   render() {
     return (
       <>
-        <Header />  
-        <Nav />
-        <Main />  
+        {/* <Header />   */}
+        {/* <Nav /> */}
+        {/* <Main />   */}
         <Footer />           
       </>      
     );
